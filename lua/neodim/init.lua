6b5844adee3ace7bd4c1e03fb3c3bd83b2e93f76
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-04-10 17:15:14 -0400

    dramatic code improvements

--- lua/neodim/init.lua
+++ lua/neodim/init.lua
@@ -3,13 +3,6 @@ local results = {}
 setmetatable(results, { __mode = "v" }) -- make values weak
 local dim = {}
 local hl_map = {}
-dim.hl = nil
-dim.timer = vim.loop.new_timer()
-
-dim.marks = {}
-dim.diag = {}
-dim.diagnostics = nil
-dim.bufnr = nil
 
 local function get_ts_group(bufnr, lnum, col, end_col)
   local ts_group = util.get_treesitter_nodes(bufnr, lnum, col, end_col)
@@ -42,11 +35,8 @@ dim.ignore_vtext = function(diagnostic)
   return not dim.detect_unused(diagnostic) and diagnostic.message or nil
 end
 
-local format_loc = function (diagnostic)
-  return {
-    start = { diagnostic.lnum, diagnostic.col },
-    ["end"] = { diagnostic.lnum, diagnostic.end_col },
-  }
+local format_loc_ext = function (diagnostic)
+  return { diagnostic.lnum, diagnostic.col }
 end
 
 dim.detect_unused = function(diagnostics)
@@ -79,29 +69,36 @@ local function create_diagnostic_extmark(bufnr, ns, diagnostic)
   })
 end
 
-local clear_extmarks = function()
-  for _, mark in ipairs(dim.marks[dim.bufnr]) do
-    vim.api.nvim_buf_del_extmark(dim.bufnr, dim.ns, mark)
+local mark_in_diagnostics = function (mark, diagnostic_locs)
+  for _, v in ipairs(diagnostic_locs) do
+    if v[1] == mark[2] and v[2] == mark[3] then return true end
   end
-  dim.marks[dim.bufnr] = {}
+  return false
 end
-local update = function ()
-  if not dim.diagnostics or not dim.bufnr then return end
-  local filtered = dim.detect_unused(dim.diagnostics)
-  dim.diag = filtered
-  if not dim.marks[dim.bufnr] then dim.marks[dim.bufnr] = {} end
-  local locs = {}
-  clear_extmarks()
-  for _, diagnostic in ipairs(dim.diag) do
-    local ext = create_diagnostic_extmark(dim.bufnr, dim.ns, diagnostic)
-    if ext then
-      table.insert(locs, format_loc(diagnostic))
-      table.insert(dim.marks[dim.bufnr], ext)
+
+local clear_extmarks = function(bufnr, diagnostics, buf_marks)
+  local diagnostic_locs = vim.tbl_map(format_loc_ext, diagnostics)
+  local new_marks = vim.tbl_filter(function(mark)
+    if mark_in_diagnostics(mark, diagnostic_locs) then return true
+    else
+      vim.api.nvim_buf_del_extmark(bufnr, dim.ns, mark[1])
+      return false
+    end
+  end, buf_marks)
+  return new_marks
+end
+
+local update = function (bufnr, filtered, buf_marks)
+  local marks_in_diagnostics = clear_extmarks(bufnr, filtered, buf_marks)
+  for _, diagnostic in ipairs(filtered) do
+    local marks_in_diagnostics_locs = vim.tbl_map(function(mark) return { mark[2], mark[3] } end, marks_in_diagnostics)
+    if not vim.tbl_contains(marks_in_diagnostics_locs, { diagnostic.lnum, diagnostic.col }) then
+      create_diagnostic_extmark(bufnr, dim.ns, diagnostic)
     end
   end
 end
 
-local create_autocmds_and_timer_start = function ()
+local create_autocmds_and_timer_start = function (timer_debounce)
   vim.api.nvim_create_autocmd({"TextYankPost"}, {
     callback = function ()
       vim.lsp.buf_notify(dim.bufnr, 'textDocument/didChange')
@@ -109,21 +106,20 @@ local create_autocmds_and_timer_start = function ()
     once = false
   })
   vim.api.nvim_create_autocmd({"InsertLeave"}, {
-    callback = function ()
-      dim.timer:start(0, 100, vim.schedule_wrap(function()
-        dim.diagnostics= dim.detect_unused(vim.diagnostic.get(dim.bufnr, {severity = vim.diagnostic.severity.HINT}))
-        update()
+    callback = vim.schedule_wrap(function ()
+      dim.timer:start(0, timer_debounce, vim.schedule_wrap(function()
+        local diagnostics = vim.diagnostic.get(0, {})
+        local buf_marks = vim.api.nvim_buf_get_extmarks(0, dim.ns, 0, -1, {})
+        clear_extmarks(vim.api.nvim_get_current_buf(), diagnostics, buf_marks)
+        vim.lsp.buf_notify(dim.bufnr, 'textDocument/didChange')
       end))
-    end,
+    end),
     once = false
   })
   vim.api.nvim_create_autocmd({"InsertEnter"}, {
-    callback = function ()
-      dim.diagnostics= dim.detect_unused(vim.diagnostic.get(dim.bufnr, {severity = vim.diagnostic.severity.HINT}))
-      vim.schedule(function()
-        dim.timer:stop()
-      end)
-    end,
+    callback = vim.schedule_wrap(function ()
+      dim.timer:stop()
+    end),
     once = false
   })
 end
@@ -134,12 +130,16 @@ dim.setup = function(params)
     vim.api.nvim_set_hl(0, "Unused", params.hl)
     dim.hl = "Unused"
   end
-  create_autocmds_and_timer_start()
+  local timer_debounce = params and params.timer_debounce or 200
+  dim.timer = vim.loop.new_timer()
+  dim.marks = {}
+  create_autocmds_and_timer_start(timer_debounce)
   vim.diagnostic.handlers["dim/unused"] = {
     show = function(_, bufnr, diagnostics, _)
-      dim.diagnostics = diagnostics
-      dim.bufnr = bufnr
-      vim.schedule(function() update() end)
+      dim.marks[bufnr] = dim.marks[bufnr] or {}
+      dim.marks[bufnr] = vim.api.nvim_buf_get_extmarks(bufnr, dim.ns, 0, -1, {})
+      local filtered = dim.detect_unused(diagnostics)
+      vim.schedule(function() update(bufnr, filtered, dim.marks[bufnr]) end)
     end
   }
 end
